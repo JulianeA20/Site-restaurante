905cd490410825a26f0456b0ec1fa439636892e8
Juliane Almeida <87144780+JulianeA20@users.noreply.github.com>
2023-04-10 19:07:59 -0300

    Quarto

--- Python/a001.py
+++
@@ -1,9 +0,0 @@
-def imprimir_linhas(n):
-    for i in range(1, n+1):
-        for j in range(1, i+1):
-            print(j, end=" ")
-        print()
-
-# Pedir ao usuário um número e chamar a função para imprimir as linhas
-numero = int(input("Digite um número inteiro: "))
-imprimir_linhas(numero)
